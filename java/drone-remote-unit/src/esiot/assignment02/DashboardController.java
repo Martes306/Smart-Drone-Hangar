a8a4dd05e90a55cf23c292d4b0b4c89b3bfc1bd0
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-05-15 11:41:34 +0200

    Refactor java part

--- java/drone-remote-unit/src/esiot/assignment02/DashboardController.java
+++ java/drone-remote-unit/src/esiot/assignment02/DashboardController.java
@@ -1,9 +1,7 @@
 package esiot.assignment02;
+
 public class DashboardController {
 
-    // static final String MSG_MAINTENANCE_DONE 	= "ok";
-	// static final String MSG_DISCHARGE 			= "di";
-	
 	SerialCommChannel channel;
 	DashboardView view;
 	LogView logger;
@@ -12,22 +10,22 @@ public class DashboardController {
 		this.view = view;
 		this.logger = logger;
 		
-		channel = new SerialCommChannel(port,115200);		
-		new MonitoringAgent(channel,view,logger).start();
+		channel = new SerialCommChannel(port, 115200);		
+		new MonitoringAgent(channel, view, logger).start();
 		
 		System.out.println("Waiting Arduino for rebooting...");		
 		Thread.sleep(4000);
 		System.out.println("Ready.");		
-	
 	}
 	
-	/*
-	public void notifyMaintenanceDone() {
-		  channel.sendMsg(MSG_MAINTENANCE_DONE);
+	public void sendTakeOff() {
+		channel.sendMsg("TAKEOFF");
+		if (logger != null) logger.log("Sent command: TAKEOFF");
 	}
 
-	public void notifyDischarge() {
-		  channel.sendMsg(MSG_DISCHARGE);
-	}*/
+	public void sendLand() {
+		channel.sendMsg("LAND");
+		if (logger != null) logger.log("Sent command: LAND");
+	}
 
 }
--- java/drone-remote-unit/src/esiot/assignment02/DashboardLauncher.java
+++ java/drone-remote-unit/src/esiot/assignment02/DashboardLauncher.java
@@ -1,30 +1,32 @@
 package esiot.assignment02;
 
-/**
- * 
- * An example of Dashboard for the Sweeping Case Study
- * 
- * Partially implemented.
- * 
- */
-class DashboardLauncher   {
+public class DashboardLauncher {
 
 	static DashboardView view = null;
 	static LogView log = null;
 
 	public static void main(String[] args) throws Exception {	
-		/*
-		if (args.length != 1){
-			System.err.println("Args: <serial port>");
-			System.exit(1);
-		}*/
+		
+		String portName = "";
+		if (args.length == 1) {
+			portName = args[0];
+		} else {
+			// fallback default port (change to match your OS, e.g., COM3 or /dev/ttyACM0)
+			portName = "/dev/ttyACM0"; 
+			System.out.println("No serial port specified, defaulting to " + portName);
+		}
 
 		view = new DashboardView();
 		log = new LogView();
 		
-		String portName = "/dev/cu.usbmodem112101";
-		DashboardController contr = new DashboardController(portName,view,log);
-		view.registerController(contr);
+		try {
+			DashboardController contr = new DashboardController(portName, view, log);
+			view.registerController(contr);
+		} catch (Exception e) {
+			System.err.println("Could not connect to serial port " + portName);
+			System.err.println("Please specify the correct port as a command line argument.");
+			// e.printStackTrace();
+		}
 		
 		view.display();
 		log.display();
